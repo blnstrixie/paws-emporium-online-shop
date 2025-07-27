b4daf8a5f0626bf5f83b34ab6a20081e05489762
Trixie Belnas <trixiebelnas7@gmail.com>
2025-07-28 04:53:19 +0800

    Initial Commit

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="styles/banner.css">
+
+    <title> Canine Safety Gear Bundles &#124; Paws Emporium </title>
+  </head>
+
+  <body>
+    <?php
+      include 'header.html'
+    ?>
+
+    <div class="banner-container">
+      <img class="banner" src="images/dogsss.jpg">
+
+      <div class="tagline">
+        Canine Safety Gear Bundles
+      </div>
+
+      <button class="shop-btn">
+        Shop All
+      </button>
+    </div>
+  </body>
+</html>
