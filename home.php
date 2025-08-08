6a330b7b2f978a4914046cf69090cd790b80cc1d
Trixie Belnas <trixiebelnas7@gmail.com>
2025-08-08 11:45:36 +0800

    Update home.php

--- home.php
+++ home.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Canine Safety Gear Bundles &#124; Paws Emporium </title>
+    <script src="https://kit.fontawesome.com/021f2ffdd5.js" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="styles/header.css">
     <link rel="stylesheet" href="styles/banner.css">
   </head>
 
