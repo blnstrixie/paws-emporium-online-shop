5d581d1f0dc8a361212e31009e3085eb3427683b
Trixie Belnas <trixiebelnas7@gmail.com>
2025-08-08 12:16:03 +0800

    Clean up HTML and fix PHP include in home.php

--- home.php
+++ home.php
@@ -5,26 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Canine Safety Gear Bundles &#124; Paws Emporium </title>
     <script src="https://kit.fontawesome.com/021f2ffdd5.js" crossorigin="anonymous"></script>
-    
+
     <link rel="stylesheet" href="styles/header.css">
     <link rel="stylesheet" href="styles/banner.css">
   </head>
 
   <body>
     <?php
-      include 'header.html'
+      include 'header.html';
     ?>
 
-    <section class="banner-container" alt="Dogs wearing safety gears">
-      <img class="banner" src="images/dogsss.jpg">
+    <main>
+      <section class="banner-container">
+        <img class="banner" src="images/dogsss.jpg" alt="Dogs wearing safety gears">
 
-      <div class="tagline">
-        Canine Safety Gear Bundles
-      </div>
+        <h1 class="tagline">
+          Canine Safety Gear Bundles
+        </h1>
 
-      <a href="#" class="shop-btn">
-        Shop All
-      </a>
-    </section>
+        <a href="#" class="shop-btn">
+          Shop All
+        </a>
+      </section>
+    </main>
   </body>
 </html>
